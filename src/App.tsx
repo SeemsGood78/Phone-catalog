389c541b323a27005e3c7d198a113cf9e667485f
Pablo <seemsgood783@gmail.com>
2026-03-17 14:19:16 +0200

    Final

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
-import React from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
+import HomePage from './pages/HomePage';
+import AccessoriesPage from './pages/AccessoriesPage';
+import PhonesPage from './pages/PhonesPage';
+import TabletsPage from './pages/TabletsPage';
+import NotFoundPage from './pages/NotFoundPage';
+import FavoritesPage from './pages/FavoritesPage';
+import CartPage from './pages/CartPage';
+import ProductDetailsPage from './pages/ProductDetailsPage';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
-
-export const App: React.FC = () => {
-  return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
-  );
-};
+export const App = () => (
+  <div className="App">
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/accessories" element={<AccessoriesPage />} />
+        <Route path="/phones" element={<PhonesPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/tablets" element={<TabletsPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+        <Route path="/phones/:id" element={<ProductDetailsPage />} />
+        <Route path="/tablets/:id" element={<ProductDetailsPage />} />
+        <Route path="/accessories/:id" element={<ProductDetailsPage />} />
+      </Routes>
+    </HashRouter>
+  </div>
+);
